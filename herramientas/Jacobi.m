43b4a9fb9db7b331519bbc68011193b47f20fa4c
ngonza27 <39801147+ngonza27@users.noreply.github.com>
2019-04-23 08:58:51 -0500

    agregando jacobi

---
+++ herramientas/Jacobi.m
@@ -0,0 +1,66 @@
+
+%matriz
+a = [45 13 -4 8;
+     -5 -28 4 -14;
+     9 15 63 -7;
+     2 3 -8 -42];
+%solucion
+b = [-25; 82; 75; -43];
+%aproximacimaciones
+x = [2;2;2;2];
+%num iteraciones
+iter = 100
+%tolerancia
+tol=1e-5
+
+cond=norm(a)*norm(a^-1);
+disp('condicional=')
+disp(cond)
+determinante=det(a);
+if determinante==0
+disp('El determinante es cero, el problema no tiene solución única')
+return
+end
+
+n=length(b);%numero de elementos del vector b
+d=diag(diag(a)); %obtencion de la matriz diagonal
+l=d-tril(a); %obtencion de la matriz diagonal superior L
+u=d-triu(a);%obtencion de la matriz diagonal inferior u
+fprintf('\n     SOLUCION:\n')
+fprintf('\nLa matriz de transicion de jacobi:\n')
+T=d^-1*(l+u); 
+disp(T)
+re=max(abs(eig(T)))
+
+if re>1
+disp('Radio Espectral mayor que 1')
+disp('el método no converge')
+return
+end
+fprintf('\nEl vector constante es::\n')
+C=d^-1*b;
+disp(C)
+i=0;
+err=tol+1;
+z=[i,x(1),x(2),x(3),err]; 
+while err>tol & i<iter
+
+  xi=T*x+C;
+%disp(xi)
+%err=norm(xi-x); %norma 2
+err=max(abs(xi-x)); %norma 1
+%err=norm(xi-x)/norm(xi); %norma relativa
+x=xi;
+
+i=i+1;
+z(i,1)=i; 
+z(i,2)=x(1); 
+z(i,3)=x(2); 
+z(i,4)=x(3);
+z(i,5)=x(4);
+z(i,6)=err;
+
+end
+fprintf('\nTABLA:\n\n    n                  x1                  x2                  x3              x4   Error\n\n   ');
+disp(z)%impresion de la tabla.
+
