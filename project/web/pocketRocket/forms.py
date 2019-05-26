433f450b6239b0f2dbe28aea3bf45153e690705d
Nicolas Gonzalez V <ngonza27@eafit.edu.co>
2019-05-26 15:22:27 -0500

    editando forms,routes y plantillas. Agregando html que faltaban

--- project/web/pocketRocket/forms.py
+++ project/web/pocketRocket/forms.py
@@ -3,7 +3,11 @@ from wtforms import StringField, SubmitField, TextAreaField
 
 class rootAlgorithms(FlaskForm):
     function = StringField('f(x)')
+    first_derivate = StringField('f`(x)')
+    second_derivate = StringField('f``(x)')
+    g_function = StringField('g(x)')
     x_0 = StringField('X0')
+    x_1 = StringField('X1')
     inter_h = StringField('h')
     inter_a = StringField('a')
     inter_b = StringField('b')
